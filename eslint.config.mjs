3464179853c6a55f7e7819f5dd3eedfa60ab32a0
AHB-7 <118571657+AHB-7@users.noreply.github.com>
2024-05-01 18:14:40 +0200

    Cards been added

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,4 +4,10 @@ import pluginJs from '@eslint/js'
 export default [
     { languageOptions: { globals: globals.browser } },
     pluginJs.configs.recommended,
+    {
+        rules: {
+            'no-unused-vars': ['warn', { vars: 'all', args: 'none' }],
+            'no-undef': 'off',
+        },
+    },
 ]
--- src/js/index.js
+++ src/js/index.js
@@ -4,3 +4,49 @@ const tooltipTriggerList = document.querySelectorAll(
 const tooltipList = [...tooltipTriggerList].map(
     (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
 )
+// Filter
+const filterEl = document.querySelector('#filter')
+const sortEl = document.querySelector('#sort')
+const filterContainer = document.querySelector('.filter-container')
+const sortContainer = document.querySelector('.sort-container')
+
+function toggleFilter() {
+    if (filterEl.checked) {
+        filterContainer.classList.remove('d-none')
+    } else {
+        filterContainer.classList.add('d-none')
+    }
+}
+
+function toggleSort() {
+    if (sortEl.checked) {
+        sortContainer.classList.remove('d-none')
+    } else {
+        sortContainer.classList.add('d-none')
+    }
+}
+
+filterEl.addEventListener('click', toggleFilter)
+sortEl.addEventListener('click', toggleSort)
+
+// Hide filter or sort container when clicking outside
+document.addEventListener(
+    'click',
+    function (event) {
+        if (
+            !filterContainer.contains(event.target) &&
+            !filterEl.contains(event.target)
+        ) {
+            filterEl.checked = false
+            toggleFilter()
+        }
+        if (
+            !sortContainer.contains(event.target) &&
+            !sortEl.contains(event.target)
+        ) {
+            sortEl.checked = false
+            toggleSort()
+        }
+    },
+    true
+) // Use capture phase to ensure this runs before other click events
