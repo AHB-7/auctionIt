02a8afdc253f50273457bfcf588e8341d392bbd2
AHB-7 <118571657+AHB-7@users.noreply.github.com>
2024-05-10 15:09:20 +0200

    path test

--- src/js/global/createcarouselitem.mjs
+++ src/js/global/createcarouselitem.mjs
@@ -21,12 +21,7 @@ export function createCarouselItem(
 
     // Create the link wrapper for the image
     const itemLink = document.createElement('a')
-    itemLink.href = checkPath(
-        'e',
-        'feed.html',
-        './singlepost.html',
-        '/auctionIt/auth/feed/singlepost.html'
-    )
+    itemLink.href = '/auth/feed/singlepost.html'
     itemLink.className = 'item-linking'
 
     // Create the image element
