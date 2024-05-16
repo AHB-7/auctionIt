e453a8d4f193e34a8fc5e1157b4db7fd6fea7590
AHB-7 <118571657+AHB-7@users.noreply.github.com>
2024-05-16 19:56:30 +0200

    fix url

--- src/js/global/signedinornot.mjs
+++ src/js/global/signedinornot.mjs
@@ -4,7 +4,7 @@ import { signedInOrNot } from './variables.mjs'
 export function checkSignedInOrNot() {
     if (getAuthToken()) {
         const loginLink = document.createElement('a')
-        loginLink.href = '../sign/registering.html'
+        loginLink.href = '/auth/profile/profile.html'
         loginLink.className =
             'bi bi-person-lines-fill fs-3 text-primary'
         loginLink.setAttribute('data-bs-toggle', 'tooltip')
@@ -13,9 +13,9 @@ export function checkSignedInOrNot() {
         signedInOrNot.appendChild(loginLink)
     } else {
         const loginLink = document.createElement('a')
-        loginLink.href = 'auth/sign/registering.html'
+        loginLink.href = '/auth/sign/registering.html'
         loginLink.className =
-            'bi bi-box-arrow-in-right fs-4'
+            'bi bi-box-arrow-in-right fs-4 pt-4'
         loginLink.setAttribute('data-bs-toggle', 'tooltip')
         loginLink.setAttribute('data-bs-placement', 'left')
         loginLink.setAttribute('data-bs-title', 'Login')
