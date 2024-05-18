00d60239e79c317f37e8768065c983379da4047b
AHB-7 <118571657+AHB-7@users.noreply.github.com>
2024-05-18 22:12:24 +0200

    the auction site is ready for validation

---
+++ src/js/profile/eventchanger.mjs
@@ -0,0 +1,51 @@
+import { dofetch } from '../auth/fetch.mjs'
+import { createCustomModal } from '../global/alertmessage.mjs'
+import { LISTING_URL } from '../global/variables.mjs'
+
+export function deletConfirmation(e) {
+    createCustomModal(
+        'Confirmation',
+        'text-success',
+        'Are you sure you want to delete this auction?',
+        'No I want',
+        '',
+        '',
+        [
+            {
+                text: 'Yes Please Delete',
+                class: 'btn-danger',
+                onClick: () => {
+                    dofetch(
+                        `${LISTING_URL}/${e}`,
+                        'DELETE',
+                        true
+                    ).then(() => {
+                        window.location.reload()
+                    })
+                },
+            },
+        ]
+    )
+}
+
+export function editConfirmation(e) {
+    createCustomModal(
+        'Confirmation',
+        'text-success',
+        'Are you sure you want to change your avatar?',
+        'No I want',
+        '',
+        '',
+        [
+            {
+                text: 'Yes Please Delete',
+                class: 'btn-danger',
+                onClick: () => {
+                    // .then(() => {
+                    //     window.location.reload()
+                    // })
+                },
+            },
+        ]
+    )
+}
